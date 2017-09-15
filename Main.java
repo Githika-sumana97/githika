bb77932ffffa1090bef06464dfc3f3a2ba71ea20
Githika-sumana97 <31958110+Githika-sumana97@users.noreply.github.com>
2017-09-15 21:40:04 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(system.in);
+		int id=sc.nextInt();
+		String fullName=sc.readLine();
+		Date birthDate=sc.next();
+		double avgMark=sc.nextInt();
+		Student(1,geethika,11.01.97,35);
+
+	}
+
+}
